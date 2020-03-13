20e5e4739d7b0e14fd5b6be3ea4e00873365f7d7
Sharnell Grant <sharnellgrant@gmail.com>
2020-03-13 02:21:19 +0000

    Done.

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -1,2 +1,23 @@
 require_relative './class_methods_module.rb'
 require_relative './dance_module.rb'
+require_relative './fancy_dance.rb'
+
+#modules allow us to collect and bundle a group of methods and make those methods available to any number of classes
+
+#include keyword allow our classes to use all of the methods of the included module as instance methods
+
+class Dancer 
+ #include Dance 
+ #extend MetaDancing
+ #below is how to create nested modules
+ 
+ extend FancyDance::ClassMethods
+  include FancyDance::InstanceMethods
+ 
+ attr_accessor :name 
+ 
+ def initialize(name)
+   @name = name 
+ end
+
+end
