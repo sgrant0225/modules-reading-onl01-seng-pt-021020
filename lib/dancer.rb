426db0dfb65147cc9cc75c110146071f33af1616
Sharnell Grant <sharnellgrant@gmail.com>
2020-03-13 02:10:43 +0000

    Automatically backed up by Learn

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -1,5 +1,6 @@
 require_relative './class_methods_module.rb'
 require_relative './dance_module.rb'
+require_relative './fancy_dance.rb'
 
 #modules allow us to collect and bundle a group of methods and make those methods available to any number of classes
 
