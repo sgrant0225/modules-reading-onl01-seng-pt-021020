b87d07e9571b0b1a4477b71356744cca041ce9a2
Sharnell Grant <sharnellgrant@gmail.com>
2020-03-13 01:36:32 +0000

    Automatically backed up by Learn

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -1,2 +1,18 @@
 require_relative './class_methods_module.rb'
 require_relative './dance_module.rb'
+
+#modules allow us to collect and bundle a group of methods and make those methods available to any number of classes
+
+#include keyword allow our classes to use all of the methods of the included module as instance methods
+
+class Dancer 
+ include Dance 
+ extend MetaDancing
+ 
+ attr_accessor :name 
+ 
+ def initialize(name)
+   @name = name 
+ end
+
+end
